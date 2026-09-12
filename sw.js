2e8fa650f729d86579ca425d2cbd58f9f97a8600
NiltonFG-PE <niltonfgn7@gmail.com>
2026-09-12 16:17:23 -0500

    Actualizar PWA con restauración bilingüe

--- sw.js
+++ sw.js
@@ -10,7 +10,7 @@
      su propia caché rápida + revalidación desde script.js.
    ============================================================ */
 
-const VERSION_APP = "v98";
+const VERSION_APP = "v99";
 const PREFIJO_CACHE = "lspedia-shell-";
 const PREFIJO_RUNTIME = "lspedia-runtime-";
 const CACHE_NOMBRE = PREFIJO_CACHE + VERSION_APP;
@@ -35,6 +35,7 @@ const ARCHIVOS_CASCARON = [
     "js/mejoras-producto-base.js",
     "js/lo-nuevo.js",
     "js/i18n.js",
+    "js/i18n-restaurar.js",
     "manifest.json",
     "img/icons/icon-192-v82.png",
     "img/icons/icon-512-v82.png",
