d137a2d4c8eb7052b54f98da782fbbdaad34ecc4
NiltonFG-PE <niltonfgn7@gmail.com>
2026-09-12 11:55:40 -0500

    Forzar carga del fullscreen con soporte para botón Atrás

--- js/security.js
+++ js/security.js
@@ -96,7 +96,7 @@
         if (document.querySelector('script[data-lspedia-fullscreen-mobile-fix]')) return;
 
         const script = document.createElement('script');
-        script.src = 'js/fullscreen-mobile-fix.js?v=20260912-2';
+        script.src = 'js/fullscreen-mobile-fix.js?v=20260912-3';
         script.async = false;
         script.dataset.lspediaFullscreenMobileFix = '1';
         document.head.appendChild(script);
