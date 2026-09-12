f6bda10030789b6d178137a6ae12477bd15ff5e7
NiltonFG-PE <niltonfgn7@gmail.com>
2026-09-12 11:49:36 -0500

    Actualizar PWA para salida estable de fullscreen

--- sw.js
+++ sw.js
@@ -10,7 +10,7 @@
    ============================================================ */
 
 // Cambia esta versión cuando modifiques el cascarón de la aplicación.
-const VERSION_APP = "v90";
+const VERSION_APP = "v91";
 const PREFIJO_CACHE = "lspedia-shell-";
 const CACHE_NOMBRE = PREFIJO_CACHE + VERSION_APP;
 
