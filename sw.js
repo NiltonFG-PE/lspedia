086dbb6268da046497f4a1cbeecb3b4906484425
NiltonFG-PE <niltonfgn7@gmail.com>
2026-09-13 17:56:50 -0500

    Renueva cache PWA para aplicar filtro por imagen

--- sw.js
+++ sw.js
@@ -12,7 +12,7 @@
      sustituya por index.html.
    ============================================================ */
 
-const VERSION_APP = "v110";
+const VERSION_APP = "v111";
 const PREFIJO_CACHE = "lspedia-shell-";
 const PREFIJO_RUNTIME = "lspedia-runtime-";
 const CACHE_NOMBRE = PREFIJO_CACHE + VERSION_APP;
