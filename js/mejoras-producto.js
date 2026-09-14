c169626fbcccade01bcc48fb9729d3883c3aab2d
NiltonFG-PE <niltonfgn7@gmail.com>
2026-09-14 05:18:27 -0500

    seo: cargar identidad institucional

--- js/mejoras-producto.js
+++ js/mejoras-producto.js
@@ -100,6 +100,7 @@
     cargarCss('css/accesibilidad-segura.css?v=20260914-1');
     cargar('js/lspedia-core.js?v=20260914-2', function(){
         cargar('js/accesibilidad-segura.js?v=20260914-1');
+        cargar('js/seo-institucional.js?v=20260914-1');
         cargar('js/juegos-banco-compartido.js?v=20260914');
         cargar('js/a-z-movil.js?v=20260914');
     });
