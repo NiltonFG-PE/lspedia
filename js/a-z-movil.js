c7e6175b86696b9d4bc28c4df21bc5113f1145e6
NiltonFG-PE <niltonfgn7@gmail.com>
2026-09-19 15:02:57 -0500

    Actualizar versión visual de grupos temáticos

--- js/a-z-movil.js
+++ js/a-z-movil.js
@@ -69,7 +69,7 @@
   if(!document.querySelector('link[data-lsp-taxonomia]')){
     const css=document.createElement('link');
     css.rel='stylesheet';
-    css.href='css/taxonomia-inteligente.css?v=20260919-1';
+    css.href='css/taxonomia-inteligente.css?v=20260919-2';
     css.dataset.lspTaxonomia='1';
     document.head.appendChild(css);
   }
