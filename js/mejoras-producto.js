7f6c9afe7f4b08ab3fe4c9f79c64a8dc4f6b600f
NiltonFG-PE <niltonfgn7@gmail.com>
2026-09-22 13:27:28 -0500

    Actualizar versión de carga de i18n

--- js/mejoras-producto.js
+++ js/mejoras-producto.js
@@ -225,7 +225,7 @@
         cargar('js/rendimiento-movil.js?v=20260914-1', function(){
             cargar('js/mejoras-producto-base.js?v=20260914', function(){
                 cargar('js/lo-nuevo.js?v=20260914');
-                cargar('js/i18n.js?v=20260914', function(){
+                cargar('js/i18n.js?v=20260922-2', function(){
                     cargar('js/i18n-restaurar.js?v=20260914', function(){
                         cargar('js/i18n-auto.js?v=20260914', function(){
                             cargar('js/i18n-nosotros.js?v=20260914', function(){
