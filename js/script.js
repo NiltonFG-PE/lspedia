bf3ca239017028e7c3c300198491f9259a061f3b
NiltonFG-PE <niltonfgn7@gmail.com>
2026-09-25 08:56:33 -0500

    Usar enlaces sociales v9 con icono de categoría

--- js/script.js
+++ js/script.js
@@ -5879,7 +5879,7 @@ function compartirColeccionVocabulario(nombre){
         .replace(/^-+|-+$/g, "");
     const url = window.location.origin
         + "/coleccion/vocabulario/" + encodeURIComponent(refColeccion)
-        + "/compartir-v8/";
+        + "/compartir-v9/";
     const titulo = coleccion + " | LSPedia";
     const texto = `Explora la colección "${coleccion}" del Vocabulario de LSPedia.`;
     if (navigator.share) {
