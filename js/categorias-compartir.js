3a0bf132e6ed4ce52e778c22d79cb32ff66a3a07
NiltonFG-PE <niltonfgn7@gmail.com>
2026-09-25 08:25:25 -0500

    Actualizar enlaces sociales a v7

--- js/categorias-compartir.js
+++ js/categorias-compartir.js
@@ -5,7 +5,7 @@
     'use strict';
 
     const CLASE_BOTON = 'btn-compartir-categoria-lspedia';
-    const VERSION_PREVIEW_SOCIAL = '20260925-6';
+    const VERSION_PREVIEW_SOCIAL = '20260925-7';
     let restauracionVocabularioRegistrada = false;
     let ultimaRestauracionConfirmada = '';
 
