25e509093a57636cf5a69ada007f6dc811f56397
NiltonFG-PE <niltonfgn7@gmail.com>
2026-09-25 07:32:49 -0500

    Renovar cache de vista previa al compartir categorías

--- js/script.js
+++ js/script.js
@@ -3178,7 +3178,7 @@ function compartirCategoriaLSPedia(nombre, fuente){
     // reciban título, descripción e imagen de esta categoría sin ejecutar JS.
     const url = window.location.origin
         + "/categoria/" + (esVocabulario ? "vocabulario" : "diccionario")
-        + "/" + encodeURIComponent(referenciaCategoria) + "/";
+        + "/" + encodeURIComponent(referenciaCategoria) + "/?v=20260925-1";
     const titulo = categoria + " | LSPedia";
     const texto = esVocabulario
         ? `Explora la categoría "${categoria}" en el Vocabulario de LSPedia.`
