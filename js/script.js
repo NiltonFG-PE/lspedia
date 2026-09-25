3f8ff137c34d3b7000782dbf900c0e6296fd6faf
NiltonFG-PE <niltonfgn7@gmail.com>
2026-09-25 10:35:58 -0500

    Actualizar versión de Carrera Matemática

--- js/script.js
+++ js/script.js
@@ -1399,7 +1399,7 @@ function abrirJuegoMatematicas(opciones = {}){
 }
 
 function abrirJuegoCarrera(){
-    window.location.assign("juegos/carrera-matematica.html?v=20260925-5");
+    window.location.assign("juegos/carrera-matematica.html?v=20260925-6");
 }
 
 const btnMenuJuegoCompletar = document.getElementById("btnMenuJuegoCompletar");
