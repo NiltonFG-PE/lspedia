bf2c530fde0641e23356b8c3a9bb75eb3c8c38de
NiltonFG-PE <niltonfgn7@gmail.com>
2026-09-25 08:56:37 -0500

    Actualizar enlaces sociales a v9

--- js/categorias-compartir.js
+++ js/categorias-compartir.js
@@ -5,7 +5,7 @@
     'use strict';
 
     const CLASE_BOTON = 'btn-compartir-categoria-lspedia';
-    const VERSION_PREVIEW_SOCIAL = 'v8';
+    const VERSION_PREVIEW_SOCIAL = 'v9';
     let restauracionVocabularioRegistrada = false;
     let ultimaRestauracionConfirmada = '';
 
