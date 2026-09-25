1f303edd71227255aa85bc5ecc18d3cf76c00ef7
NiltonFG-PE <niltonfgn7@gmail.com>
2026-09-25 12:57:11 -0500

    Actualizar acceso al tutor inteligente del chat

--- js/script.js
+++ js/script.js
@@ -1399,7 +1399,7 @@ function abrirJuegoMatematicas(opciones = {}){
 }
 
 function abrirJuegoChatEspanol(){
-    window.location.assign("juegos/chat-espanol.html?v=20260925-6");
+    window.location.assign("juegos/chat-espanol.html?v=20260925-7");
 }
 
 function abrirJuegoCarrera(){
