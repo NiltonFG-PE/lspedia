d77c0a6a3b9f17ae29955dd36f398055f4a5d6dd
NiltonFG-PE <niltonfgn7@gmail.com>
2026-09-25 11:48:01 -0500

    Actualizar acceso a Chat en Español

--- js/script.js
+++ js/script.js
@@ -1399,7 +1399,7 @@ function abrirJuegoMatematicas(opciones = {}){
 }
 
 function abrirJuegoChatEspanol(){
-    window.location.assign("juegos/chat-espanol.html?v=20260925-1");
+    window.location.assign("juegos/chat-espanol.html?v=20260925-2");
 }
 
 function abrirJuegoCarrera(){
