4195c1223459aecf7f13d33d645fe32e415283f0
NiltonFG-PE <niltonfgn7@gmail.com>
2026-09-25 08:15:35 -0500

    Actualizar versión social para apertura suave

--- js/categorias-compartir.js
+++ js/categorias-compartir.js
@@ -5,7 +5,7 @@
     'use strict';
 
     const CLASE_BOTON = 'btn-compartir-categoria-lspedia';
-    const VERSION_PREVIEW_SOCIAL = '20260925-5';
+    const VERSION_PREVIEW_SOCIAL = '20260925-6';
     let restauracionVocabularioRegistrada = false;
     let ultimaRestauracionConfirmada = '';
 
