2e308a4356d9f11d6cd6e54670fa3927a1ff41be
NiltonFG-PE <niltonfgn7@gmail.com>
2026-09-25 12:39:59 -0500

    Actualizar Chat en Español con adaptación semántica

--- js/script.js
+++ js/script.js
@@ -1399,7 +1399,7 @@ function abrirJuegoMatematicas(opciones = {}){
 }
 
 function abrirJuegoChatEspanol(){
-    window.location.assign("juegos/chat-espanol.html?v=20260925-4");
+    window.location.assign("juegos/chat-espanol.html?v=20260925-5");
 }
 
 function abrirJuegoCarrera(){
